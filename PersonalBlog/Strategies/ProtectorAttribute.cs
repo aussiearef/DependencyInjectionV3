60019860ba59998d26d08f17cd1dc40f456e4ea1
aussiearef <arefkr@gmail.com>
2019-05-08 20:55:42 +1000

    Added full code

---
+++ PersonalBlog/Strategies/ProtectorAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using PersonalBlog.Interface;
+using System;
+
+namespace PersonalBlog.Strategies
+{
+    public class ProtectorAttribute : Attribute, IActionFilter
+    {
+        private readonly IAuthorizer _authorizer;
+        public ProtectorAttribute(IAuthorizer authorizer)
+        {
+            _authorizer = authorizer;
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!_authorizer.IsAuthorized())
+            {
+                context.Result = new RedirectToActionResult("Error", "Home",null);
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
