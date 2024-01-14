fc2c510628a59dcff4290ef7809d51c706022009
aussiearef <arefkr@gmail.com>
2024-01-14 14:56:06 +1100

    Coupling-Demo-V3

---
+++ Coupling-intro/Coupling-Reduced/Program.cs
@@ -0,0 +1,80 @@
+﻿
+static void Main()
+{
+    IDataAccess dataAccess = new DataAccessMySQL();
+    IBusiness biz = new Business(dataAccess);
+    
+    var userInterface = new UserInterface(biz);
+    userInterface.Signup();
+ 
+}
+
+public interface IBusiness
+{
+    void Signup(string userName, string password);
+}
+
+public interface IDataAccess
+{
+    void Signup(string userName, string password);
+}
+
+public class UserInterface
+{
+    private string _userName = "";
+    private string _password = "";
+
+    private readonly IBusiness _business;
+
+    public UserInterface(IBusiness business)
+    {
+        _business = business;
+    }
+    private void GetData()
+    {
+        Console.Write("Enter user name:");
+        _userName = Console.ReadLine();
+
+        _password = Console.ReadLine();
+        Console.Write("Enter password:");
+    }
+
+    public void Signup()
+    {
+        GetData();
+
+        _business.Signup(_userName, _password);
+
+    }
+}
+
+public class Business : IBusiness
+{
+    private readonly IDataAccess _dataAccess;
+    public Business(IDataAccess dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+    public void Signup(string userName, string password)
+    {
+        _dataAccess.Signup(userName, password);
+    }
+}
+
+public class DataAccessSQLServer : IDataAccess
+{
+    public void Signup(string userName, string password)
+    {
+        // use EF to store data in SQL Server
+    }
+}
+
+public class DataAccessMySQL : IDataAccess
+{
+    public void Signup(string userName, string password)
+    {
+        // use EF to store data in My SQL
+    }
+}
+
+
---
+++ Coupling-intro/Coupling-intro/Program.cs
@@ -0,0 +1,51 @@
+﻿static void Main()
+{
+        
+}
+public class UserInterface
+{
+    private string _userName = "";
+    private string _password = "";
+
+    private void GetData()
+    {
+        Console.Write("Enter user name:");
+        _userName = Console.ReadLine();
+
+        _password = Console.ReadLine();
+        Console.Write("Enter password:");
+    }
+
+    public void Signup()
+    {
+        GetData();
+        var biz = new Business();
+        biz.Signup(_userName, _password);
+
+    }
+}
+
+public class Business
+{
+    public void Signup(string userName, string password)
+    {
+        var db = new DataAccessMySQL();
+        db.Signup(userName, password);
+    }
+}
+
+public class DataAccessSQLServer
+{
+    public void Signup(string userName, string password)
+    {
+        // use EF to store data in SQL Server
+    }
+}
+
+public class DataAccessMySQL
+{
+    public void Signup(string userName, string password)
+    {
+        // use EF to store data in My SQL
+    }
+}
